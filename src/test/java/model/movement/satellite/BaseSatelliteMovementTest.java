02eba16673b590896ec5186a867e4a0d87039254
Prakkmak <prakkmak@gmail.com>
2021-01-07 11:51:25 +0100

    Changement emplacement tests

--- src/test/java/model/movement/satellite/BaseSatelliteMovementTest.java
+++ src/test/java/model/movement/satellite/BaseSatelliteMovementTest.java
@@ -1,27 +1,25 @@
-package model.movement;
+package model.movement.satellite;
 
 import model.MobileElement;
-import model.movement.beacon.HorizontalMovement;
-import model.movement.beacon.decorator.BackMovement;
+import model.movement.satellite.BaseSatelliteMovement;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BackMovementTest {
-    HorizontalMovement horizontalMovement = new HorizontalMovement(0,1);
-    BackMovement movement = new BackMovement(horizontalMovement, 10);
+class BaseSatelliteMovementTest {
+
+    BaseSatelliteMovement movement = new BaseSatelliteMovement(0, 10, 1);
 
     @Test
     void shouldMoveMobileElement() {
         MobileElement mobileElement = new MobileElement(10);
-        mobileElement.setPosition(new Point(0, 0));
         movement.move(mobileElement);
-        assertEquals(1, mobileElement.getPosition().y);
+        assertEquals(1, mobileElement.getPosition().x);
     }
     @Test
-    void shouldChangeToNextMovement() {
+    void shouldGoToStartAtEnd() {
         MobileElement mobileElement = new MobileElement(10);
         mobileElement.setPosition(new Point(10,0));
         movement.move(mobileElement);
