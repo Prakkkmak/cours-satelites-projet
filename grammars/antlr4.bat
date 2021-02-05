53b90926c828ba4ae373f2e6ef5526c8323ae2ee
Prakkmak <prakkmak@gmail.com>
2021-02-05 09:28:04 +0100

    .bat

--- grammars/antlr4.bat
+++ grammars/antlr4.bat
@@ -1,2 +1,5 @@
 SET CLASSPATH=.;C:\Javalib\antlr-4.9.1-complete.jar;%CLASSPATH%
-java org.antlr.v4.Tool -visitor -package satellite.generated "grammars/$prj.g4"
+java org.antlr.v4.Tool -visitor -package generated "satellite.g4"
+del ../src/main/java/generated/*
+move *.java ../src/main/java/generated
+del *.interp .tokens
